41041d1bb6d996674f2a862e55a5942b9c248876
therealmeyer <rmeyer@scu.edu>
2018-05-17 22:51:12 -0700

    Add comments

--- W5D5/callbacks_and_functions.js
+++ W5D5/callbacks_and_functions.js
@@ -1,34 +1,37 @@
-// window.setTimeout(function () {
-//   alert('HAMMERTIME')
-// }, 5000);
-//
-// function hammertime (time) {
-//   window.setTimeout(function () {
-//     alert(`${time} is hammertime!`);
-//   });
-// }
+//Timeout
+window.setTimeout(function () {
+  alert('HAMMERTIME')
+}, 5000);
 
-// const readline = require('readline');
-//
-// const reader = readline.createInterface({
-//   input: process.stdin,
-//   output: process.stdout
-// });
-//
-// function teaAndBiscuits () {
-//   reader.question('Would you like some tea?', (res1) => {
-//     console.log(`You replied ${res1}.`);
-//     reader.question('Would you like some biscuits?', (res2) => {
-//       console.log(`You replied ${res2}.`);
-//
-//       const firstRes = (res1 === 'yes') ? 'do' : 'don\'t';
-//       const secondRes = (res2 === 'yes') ? 'do' : 'don\'t';
-//
-//       console.log(`So you ${firstRes} want tea and you ${secondRes} want biscuits.`);
-//       reader.close();
-//     });
-//   });
-// }
+//Timeout plus closure
+function hammertime (time) {
+  window.setTimeout(function () {
+    alert(`${time} is hammertime!`);
+  });
+}
+
+// I/O
+const readline = require('readline');
+
+const reader = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+function teaAndBiscuits () {
+  reader.question('Would you like some tea?', (res1) => {
+    console.log(`You replied ${res1}.`);
+    reader.question('Would you like some biscuits?', (res2) => {
+      console.log(`You replied ${res2}.`);
+
+      const firstRes = (res1 === 'yes') ? 'do' : 'don\'t';
+      const secondRes = (res2 === 'yes') ? 'do' : 'don\'t';
+
+      console.log(`So you ${firstRes} want tea and you ${secondRes} want biscuits.`);
+      reader.close();
+    });
+  });
+}
 
 // Calling functions
 function Cat () {
